38809fbbc0311928861b7ec6cc8b38683273334a
JA_X <envijs93@gmail.com>
2023-07-25 00:12:20 +0900

    Today i  solved

---
+++ CodingTest/ct129.js
@@ -0,0 +1,6 @@
+// flag에 따라 다른 값 반환하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181933
+
+function solution(a, b, flag) {
+  return flag ? a + b : a - b;
+}
---
+++ CodingTest/ct130.js
@@ -0,0 +1,7 @@
+// rny_string
+// https://school.programmers.co.kr/learn/courses/30/lessons/181863
+
+function solution(rny_string) {
+  const jokeStr = rny_string.replace(/m/g, "rn");
+  return jokeStr;
+}
