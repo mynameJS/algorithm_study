32fecdf3ca804e47e1f66b163c2f66f994562ea5
JA_X <envijs93@gmail.com>
2023-07-26 00:06:00 +0900

    Today i solved

---
+++ CodingTest/ct131.js
@@ -0,0 +1,6 @@
+// 문자열 곱하기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181940
+
+function solution(my_string, k) {
+  return my_string.repeat(k);
+}
---
+++ CodingTest/ct132.js
@@ -0,0 +1,6 @@
+// 소문자로 바꾸기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181876
+
+function solution(myString) {
+  return myString.toLowerCase();
+}
