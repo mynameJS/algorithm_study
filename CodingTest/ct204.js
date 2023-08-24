dd76615f322719e92974eb6331ea869f299feb37
JA_X <envijs93@gmail.com>
2023-08-24 12:33:45 +0900

    Today i solved

---
+++ CodingTest/ct204.js
@@ -0,0 +1,10 @@
+// 문자열 뒤집기
+// https://school.programmers.co.kr/learn/courses/30/lessons/181905
+
+function solution(my_string, s, e) {
+  return (
+    my_string.slice(0, s) +
+    [...my_string.slice(s, e + 1)].reverse().join("") +
+    my_string.slice(e + 1)
+  );
+}
